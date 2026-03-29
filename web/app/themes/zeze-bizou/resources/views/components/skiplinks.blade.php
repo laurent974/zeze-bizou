26fd80af5ea1622aac3e92d856cbfad10997ae04
Yen-Pon Laurent <lyenpon974@gmail.com>
2026-03-29 14:50:09 +0200

    Del - Réinitialisation project

--- web/app/themes/zeze-bizou/resources/views/components/skiplinks.blade.php
+++
@@ -1,9 +0,0 @@
-<div class="skiplinks">
-  <nav role="navigation" aria-label="{{ __('Quick access') }}" class="container">
-    <ul class="skiplinks-list">
-      <li>
-        <a class="link" href="#main">{{ __('Content') }}</a>
-      </li>
-    </ul>
-  </nav>
-</div>
